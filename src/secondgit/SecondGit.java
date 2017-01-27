21926677617a38c568e2540e7f13533e820f48b0
agnas <agnas@L11704042767.wctc.msft>
2017-01-26 21:47:55 -0600

    Added a new variable

---
+++ src/secondgit/SecondGit.java
@@ -0,0 +1,12 @@
+
+package secondgit;
+
+public class SecondGit {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int x;
+        int y;
+    }
+    
+}
